09fd80efbca053bb2d45d21ac83b7af59bd78cd5
Andri <and@ri.ri>
2016-11-05 13:40:00 +0200

    made the code prettier

--- cameras.py
+++ cameras.py
@@ -1,41 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # cameras colour script
-reset = '\x1B[0m'
-default = '\x1B[39m'
-black = '\x1B[30m'
-red = '\x1B[31m'
-green = '\x1B[32m'
-yellow = '\x1B[33m'
-blue = '\x1B[34m'
-magenta = '\x1B[35m'
-cyan = '\x1B[36m'
-lightgray = '\x1B[38m'
-darkgray = '\x1B[90m'
-lightred = '\x1B[91m'
-lightgreen = '\x1B[92m'
-lightyellow = '\x1B[93m'
-lightblue = '\x1B[94m'
-lightmagenta = '\x1B[95m'
-lightcyan = '\x1B[96m'
-white = '\x1B[97m'
+
+reset = "\x1B[0m"
+default = "\x1B[39m"
+black = "\x1B[30m"
+red = "\x1B[31m"
+green = "\x1B[32m"
+yellow = "\x1B[33m"
+blue = "\x1B[34m"
+magenta = "\x1B[35m"
+cyan = "\x1B[36m"
+lightgray = "\x1B[38m"
+darkgray = "\x1B[90m"
+lightred = "\x1B[91m"
+lightgreen = "\x1B[92m"
+lightyellow = "\x1B[93m"
+lightblue = "\x1B[94m"
+lightmagenta = "\x1B[95m"
+lightcyan = "\x1B[96m"
+white = "\x1B[97m"
 
 shape = [
-u''*12,
-u'    ████    ',
-u' █▀██▀▀████ ',
-u' ███ ██ ███ ',
-u' ████▄▄████ ',
-u''*12]
+    u"            ",
+    u"    ████    ",
+    u" █▀██▀▀████ ",
+    u" ███ ██ ███ ",
+    u" ████▄▄████ ",
+    u"            "
+]
 
 coloursets = [
-[yellow, red, green, blue, magenta, cyan],
-[lightyellow, lightred, lightgreen, lightblue, lightmagenta, lightcyan]]
+    [yellow, red, green, blue, magenta, cyan],
+    [lightyellow, lightred, lightgreen, lightblue, lightmagenta, lightcyan]
+]
 
 for colours in coloursets:
     print
     for line in shape:
-	for colour in colours:
-	    print colour+line,
-	print
+        for colour in colours:
+            print colour + line,
+        print
 print reset
